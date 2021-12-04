5b44981ba96b470e70d6aec7b65c0273b1a9cd0e
Sascha <sasha.bubnov.k@gmail.com>
2021-12-04 19:40:18 +0300

    fixes, start card

--- #src/js/script.js
+++ #src/js/script.js
@@ -1,2 +1,25 @@
 $(document).ready(function () {
+    var image = document.getElementsByClassName('parallax');
+    console.log(image)
+    paralax = new simpleParallax(image, {
+        delay: 0,
+        scale: 1.1,
+        overflow: true,
+        orientation: 'down',
+     });
+
+
+
+     Fancybox.bind("a[data-fancybox]", {
+        autoFocus: false,
+        trapFocus: false
+    });
+
+    $('.dropdown').on("mouseenter", function(){ 
+      $('.dropdown-toggle', this).trigger('click'); 
+    });
+
+    $('.dropdown').on("mouseleave", function(){ 
+      $('.dropdown-toggle', this).trigger('click'); 
+    });
 })
